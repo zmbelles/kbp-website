3ae01025c733602d88447bd1ed17a3d7823ea4c8
Zachary Belles <91072653+zmbelles@users.noreply.github.com>
2025-10-18 23:14:40 -0500

    add 2025 folder
    add 2025 podiums folder to site/pictures with podium pictures for Zach and Alex this year

--- kbp-powersport-website/src/router.js
+++ kbp-powersport-website/src/router.js
@@ -3,12 +3,14 @@ import Home from './views/Home.vue'
 import Story from './views/OurStory.vue'
 import Blog from './views/OurBlog.vue'
 import Pictures from './views/OurPictures.vue'
+import Pictures2025 from './views/Pictures2025.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/story', name: 'Story', component: Story },
   { path: '/blog', name: 'Blog', component: Blog },
   { path: '/pictures', name: 'Pictures', component: Pictures },
+  { path: '/pictures/2025', name: 'Pictures2025', component: Pictures2025 },
 ]
 
 const router = createRouter({
